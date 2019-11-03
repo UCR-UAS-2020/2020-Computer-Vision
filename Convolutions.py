f02687904eb832ce98d96f4db1b78aaf1d178ff7
ceng002 <ceng002@ucr.edu>
2019-11-03 13:17:19 -0800

    Update Convolutions.py

--- Convolutions.py
+++ Convolutions.py
@@ -1,9 +1,8 @@
-# import the necessary packages
+#import the necessary packages
 from skimage.exposure import rescale_intensity
 import numpy as np
 import argparse
 import cv2
-
 
 def convolve(image, kernel):
     # grab the spatial dimensions of the image, along with
@@ -15,8 +14,7 @@ def convolve(image, kernel):
     # "pad" the borders of the input image so the spatial
     # size (i.e., width and height) are not reduced
     pad = (kW - 1) // 2
-    image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
-                               cv2.BORDER_REPLICATE)
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
     output = np.zeros((iH, iW), dtype="float32")
 
     # loop over the input image, "sliding" the kernel across
@@ -32,36 +30,10 @@ def convolve(image, kernel):
             # perform the actual convolution by taking the
             # element-wise multiplicate between the ROI and
             # the kernel, then summing the matrix
-            k = (roi * kernel).sum() / 159
+            k = (roi * kernel).sum()
 
             # store the convolved value in the output (x,y)-
             # coordinate of the output image
             output[y - pad, x - pad] = k
 
-    # rescale the output image to be in the range [0, 255]
-    output = rescale_intensity(output, in_range=(0, 255))
-    output = (output * 255).astype("uint8")
 
-    # return the output image
-    return output
-
-kernel = np.array((
-    [0, 1, 0],
-    [1, 4, 1],
-    [0, 1, 0]))
-
-kernel1 = np.array((
-    [2, 4,  5,   4, 2],
-    [4, 9,  12,  9, 4],
-    [5, 12, 15, 12, 5],
-    [4,  9, 12,  9, 4],
-    [2,  4,  5,  4, 2]
-))
-img1 = cv2.imread(r'C:\Users\chris\Desktop\Labrador-Retriever-MP.jpg',0)
-
-img2 = convolve(img1, kernel1)
-
-img12 = np.hstack((img1,img2))
-
-cv2.imshow('lol', img12)
-cv2.waitKey()
