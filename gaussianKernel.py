9a1d1da35d5c5ab6bfcc4ad0a69812cfa9599d74
tenorchops04 <33356170+tenorchops04@users.noreply.github.com>
2019-11-18 16:17:21 -0800

    Guassian Kernel
    A function which creates a kernel with size "size" and sigma "sigma". I am not sure if the values actually stored in the kernel are correct yet, but other than that, I think it is functional.

---
+++ gaussianKernel.py
@@ -0,0 +1,27 @@
+import numpy as np
+import math
+
+def gaussKern(size, sigma):
+    output = []         # The 2D array that represents the Guassian Kernel.
+
+    # Dynamically populates the 2D array with the result of applying the 2D Gaussian function to each index in the
+    # kernel.
+    for r in range(1, size + 1):
+        row = []    # Holds the values for each element in a row of size 'size'.
+
+        # Populates the row array with the 2D Gaussian function results.
+        for c in range(1, size + 1):
+            arrElem = (1/((2 * math.pi)*(sigma**2))) * math.exp(-((r**2+c**2)/(2*(sigma**2))))
+            row.insert(c, arrElem)
+
+        # Insert the row array into the output array.
+        output.insert(r, row)
+
+    return output
+
+gKern = gaussKern(5, 2.5)
+
+for r in gKern:
+    for c in r:
+        print(c)
+    print()
