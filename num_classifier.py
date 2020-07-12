bedc167cbd3089a21665899eeb53e47d2b055d95
hscottvo <hscottvo@gmail.com>
2020-07-11 20:46:03 -0700

    Update num_classifier.py
    Fix broken import, PEP858 styling

--- num_classifier.py
+++ num_classifier.py
@@ -6,9 +6,10 @@ from keras.layers import BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.datasets import mnist
-from keras.utils import load_model
+from keras.models import load_model
 from keras.utils import to_categorical
 from sklearn.model_selection import KFold
+
 
 def load_dataset():
     (trainX, trainY), (testX, testY) = mnist.load_data()
@@ -18,12 +19,14 @@ def load_dataset():
     testY = to_categorical(testY)
     return trainX, trainY, testX, testY
 
+
 def prep_pixels(train, test):
     train_norm = train.astype('float32')
     test_norm = test.astype('float32')
     train_norm = train_norm / 255.0
     test_norm = test_norm / 255.0
     return train_norm, test_norm
+
 
 def define_model():
     model = Sequential()
@@ -45,6 +48,7 @@ def define_model():
                   metrics=['accuracy'])
     return model
 
+
 def evaluate_model(dataX, dataY, n_folds=5):
     scores, histories = list(), list()
     kfold = KFold(n_folds, shuffle=True, random_state=1)
@@ -63,10 +67,12 @@ def evaluate_model(dataX, dataY, n_folds=5):
         histories.append(history)
     return scores, histories
 
+
 def pre_run_prep():
     trainX, trainY, testX, testY = load_dataset()
     trainX, testX = prep_pixels(trainX, testX)
     return trainX, trainY, testX, testY
+
 
 def test_harness():
     trainX, trainY, testX, testY = pre_run_prep()
@@ -81,9 +87,14 @@ def test_harness():
               verbose=0)
     model.save('final_model.h5')
 
+
 def load_run_model():
     trainX, trainY, testX, testY = pre_run_prep()
     trainX, testX = pre_run_prep()
     model = load_model('final_model.h5')
     _, acc = model.evaluate(testX, testY, verbose=0)
     print('> %.3f' % (acc * 100.0))
+
+
+if __name__ == "__main__":
+    test_harness()
