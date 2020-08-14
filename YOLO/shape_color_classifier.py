68ec92e7f16704c06a4d4b4fdce959cec26ab209
Kashyap Panda <kashyap.panda@gmail.com>
2020-08-13 22:44:47 -0700

    Added shape color classifier
    Outputs color number based on input image

--- YOLO/shape_color_classifier.py
+++ YOLO/shape_color_classifier.py
@@ -1,10 +1,13 @@
-from keras.layers import Dense
-from keras.layers import Conv2D
-from keras.layers import Flatten
-from keras.layers import MaxPooling2D
-from keras.layers import Activation
-from keras.models import Sequential
-# from keras.utils import to_categorical
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array
+from tensorflow.keras.optimizers import Adam
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
 
 import json
 import numpy as np
@@ -15,68 +18,146 @@ import pickle
 from tqdm import tqdm
 from proto import *
 
-IMAGE_SIZE = 50
-img_path = r'..\..\ImageGenerator\Targets'
-target_path = r'..\..\ImageGenerator\Target Data'
+IMAGE_SIZE = 96
+img_path = '../../ImageGenerator/Targets'
+target_path = '../../ImageGenerator/Target Data'
+json_path = '../../ImageGenerator/Target Data'
 
+EPOCHS = 1
+INIT_LR = 1e-3
+BS = 32
+IMAGE_DIMS = (96, 96, 3)
+
+data = []
+labels = []
+lb = LabelBinarizer()
 
 def create_training_data(image_path, json_path, img_size):
+    global data
+    global labels
+
     training_data = []
+    training_labels = []
+
+    random.seed(27)
+
     for img in tqdm(os.listdir(image_path)):
-        img_array = cv2.imread(os.path.join(image_path, img))
-        new_array = cv2.resize(img_array, (img_size, img_size))
-        json_file = img[0:-4] + '.json'
-        json_dict = json.load(open(os.path.join(json_path, json_file)))
-        class_number = Color[json_dict['shape_color']].value
 
-        training_data.append([new_array, class_number])
+        if ( "DS" in os.path.join(image_path, img)):
+            print(".DS_STORE DETECTED")
+        else:
+            image = cv2.imread(os.path.join(image_path, img))
+            image = cv2.resize(image, (img_size, img_size))
+            image = img_to_array(image)
+            training_data.append(image)
 
-        random.shuffle(training_data)
+            json_file = img[0:-4] + '.json'
 
-    return np.array(training_data)
+            if (json_file != '.DS_S.json'):
+                json_dict = json.load(open(os.path.join(json_path, json_file)))
+                class_number = Color[json_dict["shape_color"]].value
+                training_labels.append(class_number)
 
+                random.shuffle(training_data)
+                random.shuffle(training_labels)
 
-def store_training_data():
-    X_data = []
-    y_data = []
-    shape_training_data = create_training_data(img_path, target_path, IMAGE_SIZE)
-    for feature, label in shape_training_data:
-        X_data.append(feature)
-        y_data.append(label)
+    # print(training_data)
+    data = np.array(training_data, dtype="float") / 255.0
+    labels = np.array(training_labels)
 
-    X = np.array(X_data)
-    X = X.reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 1)
-    y = np.array(y_data)
+    print("[INFO] data matrix: {:.2f}MB".format(data.nbytes / (1024 * 1000.0)))
 
-    pickle_out = open(r"Training_Data/shape_color_X.pickle", "wb")
-    pickle.dump(X, pickle_out)
-    pickle_out.close()
+    labels = lb.fit_transform(labels)
 
-    pickle_out = open(r"Training_Data/shape_color_y.pickle", "wb")
-    pickle.dump(y, pickle_out)
-    pickle_out.close()
-
-
-def define_model():
+# classes should be 10 for 10 models
+def create_model(width, height, depth, classes):
     model = Sequential()
+    inputShape = (height, width, depth)
+    chanDim = -1
 
-    model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
-    model.add(Activation('relu'))
+    if K.image_data_format == "channels_first":
+        inputShape = (depth, height, width)
+        chanDim = 1
+
+    model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(Conv2D(64, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    model.add(Conv2D(256, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Conv2D(128, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(Conv2D(128, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=chanDim))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
 
-    model.add(Dense(64))
-
-    model.add(Dense(14))
-    model.add(Activation('sigmoid'))
-
-    model.compile(loss='sparse_categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    model.add(Dense(classes))
+    model.add(Activation("softmax"))
 
     return model
+
+def model_training_data():
+    (xTrain, xTest, yTrain, yTest) = train_test_split(data, labels, test_size=0.2, random_state=27)
+
+    # Adds more data by rotating/shifting/shearing etc.
+    # DON'T USE ON WEAK COMPUTERS
+    aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
+
+    model = create_model(96, 96, 3, len(lb.classes_))
+    opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+    print("TRAINING NETWORK...")
+    model.fit(xTrain, yTrain, validation_data=(xTest, yTest), steps_per_epoch=len(xTrain) // BS, epochs=EPOCHS, verbose=1)
+
+    print("SAVING MODEL...")
+    model.save('m.model', save_format="h5")
+    f = open(r"Training_Data/color.pickle", "wb")
+    f.write(pickle.dumps(lb))
+    f.close()
+
+def test_model(imagePath):
+
+    image = cv2.imread(imagePath)
+
+    image = cv2.resize(image, (96, 96))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    model = load_model('m.model')
+    lb = pickle.loads(open('Training_Data/color.pickle', 'rb').read())
+
+    print("Classifying Image...")
+    prediction = model.predict(image)[0]
+    idx = np.argmax(prediction)
+    label = lb.classes_[idx]
+
+    print("RESULT:")
+    print(label)
+
+
+# Main function used for testing purposes
+# if __name__ == '__main__':
+#     training_data = create_training_data(img_path, json_path, 96)
+#     model_training_data()
+#     test_model(os.path.join(img_path, '0.png'))
+
